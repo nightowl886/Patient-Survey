b44514cdfb8b7370c9299d88823caf2585cecaa1
Mengwan Wang <zapdoschia@gmail.com>
2026-08-06 17:04:24 -0400

    Update Data exploration for hospital_beds dataset.sql

--- Data exploration.sql
+++ Data exploration.sql
@@ -1,9 +1,24 @@
 set datestyle = 'SQL,MDY'
 
-select  provider_ccn,
+with hospital_beds_prep as
+(
+
+select  lpad(cast(provider_ccn as text),6,'0') as provider_ccn,
         hospital_name,
 	    to_date(fiscal_year_begin_date,'YYYY/MM/DD') as fiscal_year_begin_date,
 		to_date(fiscal_year_end_date,'YYYY/MM/DD') as fiscal_year_end_date,
-		number_of_beds
+		number_of_beds,
+		row_number() over (
+             partition by provider_ccn
+			 order by to_date(fiscal_year_end_date,'YYYY/MM/DD') DESC) as nth_row
 from "freewitch"."Hospital_Data".hospital_beds
+)
+
+	
+select provider_ccn, count(*) as count_of_rows
+
+from hospital_beds_prep
+where nth_row = 1
+group by provider_ccn
+order by count(*) DESC
 
